584d1daa1f483c2b50585977c530033280e9f2a9
Edel Abreu <edel.abreu@upr.edu.cu>
2024-12-23 17:03:56 +0200

    added internationalization

--- vocabs/frontend/app.py
+++ vocabs/frontend/app.py
@@ -1,7 +1,23 @@
 import streamlit as st
+import gettext
+
+gettext.bindtextdomain("messages", "vocabs/frontend/locales")
+gettext.textdomain("messages")
+
+language = st.sidebar.selectbox(' ', ['es', 'en'])
+
+translations = gettext.translation("messages", localedir="vocabs/frontend/locales", languages=[language])
+
+translations.install()
 
 
-st.title('Sceiba Vocabularies Service')
-st.subheader('bla bla bla ')
+_ = translations.gettext
 
-st.text_area("hola streamlit")
+
+st.write(_('This is a translatable string.'))
+st.write(_('This is a text'))
+
+st.title(_('Sceiba Vocabularies Service'))
+st.subheader('System for vocabularies')
+
+st.text_area(_("Hello streamlit"))
